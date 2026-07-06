9495e12573165b50976d7d4dea59b5806cacc343
CarolineXu01 <s4790970@student.uq.edu.au>
2026-07-07 00:27:27 +1000

    Updates

--- _next/static/u1MnSw7eWT0NCkXLG_qRL/_buildManifest.js
+++ _next/static/u1MnSw7eWT0NCkXLG_qRL/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/carolinexu-portfolio//_next/:path+",
+        "destination": "/carolinexu-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
